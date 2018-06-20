09ab03463bdd23521dffeae4e3b5a6b78b55e0dc
Sandy Dimpu <sandydimpu@Sandys-MacBook-Pro.local>
2018-06-20 18:29:20 -0500

    Integrated Edit and Update Actions for Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,12 +18,28 @@ def create
        # format.json { render :show, status: :created, location: @portfolio_items }
       else
         format.html { render :new }
-       # format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+       # format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
 
 end
 
- 
+def edit
+	@portfolio_item = Portfolio.find(params[:id])
+end
+
+def update
+	@portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+end
 
 end
