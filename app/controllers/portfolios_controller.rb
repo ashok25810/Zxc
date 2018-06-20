6db82f6ae967c55651f3fde251c09df7590ded1b
Sandy Dimpu <sandydimpu@Sandys-MacBook-Pro.local>
2018-06-20 18:04:06 -0500

    Integrated new nd create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,13 +1,29 @@
 class PortfoliosController < ApplicationController
 
 def index
+	@portfolio_item = Portfolio.all
+end
 
-	@portfolio_items = Portfolio.all
+def new 
+@portfolio_item = Portfolio.new
+end
+
+def create 
+
+	 @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+       # format.json { render :show, status: :created, location: @portfolio_items }
+      else
+        format.html { render :new }
+       # format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
 
 end
 
-
-
-
+ 
 
 end
