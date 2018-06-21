c06603e6d69af1ef802008b725ebe02a7249733e
Sandy Dimpu <sandydimpu@Sandys-MacBook-Pro.local>
2018-06-20 19:41:50 -0500

    Destroy action added to portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -46,4 +46,14 @@ def show
 	@portfolio_item = Portfolio.find(params[:id])
 end
 
+def destroy
+	@portfolio_item = Portfolio.find(params[:id])
+
+	@portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+end
+
 end
